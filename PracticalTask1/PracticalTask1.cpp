33bf6a0385d01524986e6e6cfaeef2f5dca0761d
Небытов Даниил <danil2002.11@mail.ru>
2021-02-16 03:07:32 +0500

    Первая лаба

---
+++ PracticalTask1/PracticalTask1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+void CalculateBigInt(int num) 
+{
+    char bytes[4];
+    for (int i = 0; i < 4; i++)
+    {
+        num >>= 8 * i;
+        bytes[i] = num & 15;
+    }
+    cout << "Big-Endian:";
+    for (int k = 3; k >= 0; k--)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            cout << ((bytes[k] & 128) >> 7);
+            bytes[k] <<= 1;
+        }
+    }
+}
+void CalculateLittleInt(int num)
+{
+    char bytes[4];
+    for (int i = 0; i < 4; i++)
+    {
+        num >>= 8 * i;
+        bytes[i] = num & 15;
+    }
+    cout << "Little-Endian:";
+    for (int k = 0; k < 4; k++)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            cout << ((bytes[k] & 128) >> 7);
+            bytes[k] <<= 1;
+        }
+    }
+}
+int main()
+{
+    cout << "Input your number: ";
+    double num;
+    cin >> num;
+    cout << endl;
+    cout << "int:" << endl;
+    CalculateBigInt((int)num);
+    cout << endl;
+    CalculateLittleInt((int)num);
+}
